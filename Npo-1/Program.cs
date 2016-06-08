c23b1ec3234aab116aab0d729deb440e32732ddd
Q2key <dimatocky@gmail.com>
2016-06-08 17:21:07 +0400

    fix

--- Npo-1/Program.cs
+++ Npo-1/Program.cs
@@ -13,6 +13,7 @@ namespace Npo_1
         {
             var m = new Matrix();
             var array = m.ReadArrayFromFile(@"D:\input.txt");
+            /*
             foreach (var arr in array)
             {
                 foreach (var a in arr)
@@ -20,21 +21,20 @@ namespace Npo_1
                     Console.Write(@" {0} ",a);
                 }
                 Console.WriteLine();
-            }
-            
-
+            }*/
+            var a = new int[] { 1, 2, 3, 4 };
+            var a2 = new int[] { 9, 11, 8, 10 };
             Console.ReadKey();
         }
     }
 
     class Matrix
     {
-        
         public int[][] ReadArrayFromFile(string path)
         {
             var colandrow = File.ReadAllLines(path).Skip(0).First();
-            var columns = int.Parse(colandrow.Split(' ')[0]);
-            var rows = int.Parse(colandrow.Split(' ')[1]);
+            var columns = int.Parse(colandrow.Split()[0]);
+            var rows = int.Parse(colandrow.Split()[1]);
             var sarray = File.ReadAllLines(path).Skip(1).ToArray();
             var a = new int[columns][];
             for (var i = 0; i < a.Length; i++)
@@ -44,6 +44,17 @@ namespace Npo_1
             }
             return a;
         }
-        
+        public int GetMonosequenceLenght(int[] array)
+        {
+            var msl = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+
+            }
+
+            return msl;
+        }
+
+
     }
 }
